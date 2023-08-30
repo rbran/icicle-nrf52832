fdeccd1159a8edbb53651f5d29f9276cba0919c9
Rubens Brandao <git@rubens.io>
2023-08-30 12:03:03 -0300

    Include basic documentation

---
+++ Readme.md
@@ -0,0 +1,7 @@
+# Icicle Nrf52832
+
+Example of partially implemented VM for the
+[nrf52832](https://www.nordicsemi.com/products/nrf52832) generated using using
+the project
+[svd2icicle](https://github.com/rbran/svd2icicle), the svd files from
+[nrd-pacs](https://github.com/nrf-rs/nrf-pacs).
--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,13 @@ use icicle_nrf52832::{map_cpu, peripheral::Peripherals};
 
 fn main() {
     let cpu_config =
-        icicle_vm::cpu::Config::from_target_triple("i586-linux-musl");
+        icicle_vm::cpu::Config::from_target_triple("thumbv7m-unknown-unknown");
     let mut vm = icicle_vm::build(&cpu_config).unwrap();
-
     vm.env = icicle_vm::env::build_auto(&mut vm).unwrap();
 
     let peripheral = Arc::new(Mutex::new(Peripherals::default()));
     map_cpu(&peripheral, &mut vm.cpu);
+
+    // Load binary and run the vm...
     todo!();
 }
